3b2e5998bbb1bce1841861cd41a8d59548aa8de9
Hristos Giannopoulos <hristos@outlook.com>
2015-02-01 19:28:53 -0500

    Adds snake print diagnostic function

--- embedded/led_clock.ino
+++ embedded/led_clock.ino
@@ -13,6 +13,7 @@ ht1632c ledMatrix = ht1632c(&PORTB, DATA, WR, CLK, CS, GEOM_32x16, 1);
 game_struct_t gameStruct;
 snake_struct_t snakeStruct;
 
+int snake_print(void * cookie, const char *format, ...);
 int ht1632c_plot(void* cookie, uint8_t X, uint8_t Y, uint32_t color);
 int ht1632c_clear(void* cookie); 
 int ht1632c_push(void * cookie);
@@ -42,22 +43,51 @@ void setup() {
     gameStruct.plot_function  = ht1632c_plot;
     gameStruct.clear_function = ht1632c_clear;
     gameStruct.push_function  = ht1632c_push;
+    gameStruct.print_function = snake_print;
     gameStruct.cookie         = NULL;
     gameStruct.board_x        = 31;
     gameStruct.board_y        = 15;
     gameStruct.color_depth    = 2;
 
+    /* 8 3 5 5 1 Efficient - gets in loop */
+    /* 8 1 2 5 1 Efficient gets in loop */
+    /* 7 7 8 4 1 Pretty efficcient no loop*/
+    /* 5 6 1 2 3 Likes to loop on iteself gets into loop */
+
+    gameStruct.manhattan_weight    = 1;
+    gameStruct.turn_weight         = 1;
+    gameStruct.no_turn_weight      = 1;
+    gameStruct.look_ahead_distance = 1;
+    gameStruct.look_ahead_weight   = 1;
+
     snakeInit(&gameStruct, &snakeStruct);
+    
+    snakeStruct.snake_color = 1;
+    snakeStruct.apple_color = 2;
 
 }
 
 void loop() {
     runFrame();
     drawSnake();
+    if(snakeStruct.state == WON){
+        Serial.println("WON");
+        //Serial.println(snakeStruct->apple_count);
+    }
+    else if(snakeStruct.state == DEAD){
+        snake_print(NULL, "LOST %u \n\r", snakeStruct.apple_count);
+    }
+
+    // Serial.print("HEAD - ");
+    // Serial.print(GET_X(snakeStruct.array[snakeStruct.length -1]));
+    // Serial.print(" - ");
+    // Serial.println(GET_Y(snakeStruct.array[snakeStruct.length -1]));
+
     /*printStruct(&snakeStruct);*/
     ledMatrix.sendframe();
-    Serial.println("Frame Ran");
-    delay(20);
+    //Serial.println(snakeStruct.state);
+
+    //delay(20);
 }
 
 int ht1632c_plot(void* cookie, uint8_t x, uint8_t y, uint32_t color){
@@ -71,5 +101,17 @@ int ht1632c_clear(void* cookie){
 } 
 int ht1632c_push(void * cookie){
     ledMatrix.sendframe();
+    return 1;
+}
+
+int snake_print(void * cookie, const char *format, ...){
+    char buffer[SPRINTF_BUFF_LEN] ="";
+    va_list argptr;
+    va_start(argptr, format);
+
+    sprintf(buffer, format, argptr);
+
+    va_end(argptr);
+    Serial.print(buffer);
     return 1;
 }
